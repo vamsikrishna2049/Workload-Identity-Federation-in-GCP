e71abab74b795d1dab4c966a56dbdf06568f1520
Vamsi krishna <118727057+vamsikrishna2049@users.noreply.github.com>
2026-04-16 11:13:34 +0530

    Provider file

--- provider.tf
+++ provider.tf
@@ -18,3 +18,21 @@ terraform {
     prefix = "terraform/state"
   }
 }
+
+
+locals {
+  required_apis = [
+    "compute.googleapis.com",        # VM, VPC, firewall, routes
+    "storage.googleapis.com",        # Cloud Storage
+    "dns.googleapis.com",            # Cloud DNS
+  ]
+}
+
+resource "google_project_service" "apis" {
+  for_each = toset(local.required_apis)
+
+  project = var.project_id
+  service = each.value
+
+  disable_on_destroy = false
+}
